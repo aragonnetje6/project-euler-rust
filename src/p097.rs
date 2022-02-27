d028142702a0bafbdc4e5348a4b0f4c717675c94
aragonnetje6 <twanstok@gmail.com>
2022-02-27 22:48:50 +0100

    optimized

--- src/p097.rs
+++ src/p097.rs
@@ -1,10 +1,6 @@
 use mod_exp::mod_exp;
 
 pub fn p097() {
-    let x = mod_exp(2 as u128, 7830457 as u128, 10000000000 as u128);
-    let mut y = 0;
-    for _i in 0..28433 {
-        y = (y + x) % 10000000000;
-    }
-    println!("answer: {}", y + 1)
+    let x = (mod_exp(2 as u128, 7830457 as u128, 10000000000 as u128) * 28433 + 1) % 10000000000;
+    println!("answer: {}", x)
 }
