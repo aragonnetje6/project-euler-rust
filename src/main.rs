70403337f4fac3a9e065ccbc3aa50cd11b3cd36c
aragonnetje6 <twanstok@gmail.com>
2022-03-01 05:26:13 +0100

    solved p085

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@
 // use crate::p096::{p096};
 // use crate::p097::{p097};
 // use crate::p074::{p074};
-use crate::p075::p075;
+// use crate::p075::{p075};
+use crate::p085::p085;
 
 mod p069;
 mod p070;
@@ -19,13 +20,15 @@ mod p075;
 mod p096;
 mod p097;
 mod primes_and_phi;
+mod p085;
 
 fn main() {
     // p069();
     // p070();
     // p071();
     // p074();
-    p075();
+    // p075();
+    p085();
     // p096();
     // p097();
 }
---
+++ src/p085.rs
@@ -0,0 +1,25 @@
+pub fn p085() {
+    let mut answers = get_all_areas(100);
+    answers.sort_by_cached_key(|(_w, _h, n, _a)| { n.abs_diff(2000000) });
+    println!("n: {}, a: {}", answers.first().expect("error").clone().2, answers.first().expect("error").clone().3)
+}
+
+fn get_rects_for(width: u32, height: u32) -> u32 {
+    let mut total = 0;
+    for i in 0..width {
+        for j in 0..height {
+            total += (width - i) * (height - j);
+        }
+    }
+    return total;
+}
+
+fn get_all_areas(max_width: u32) -> Vec<(u32, u32, u32, u32)> {
+    let mut out: Vec<(u32, u32, u32, u32)> = Vec::new();
+    for width in 2..max_width {
+        for height in 1..width {
+            out.push((width, height, get_rects_for(width, height), width * height));
+        }
+    }
+    return out;
+}
